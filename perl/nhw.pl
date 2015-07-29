5052007d80afb16c073065ad4567445e5dcd7dfd
Neil Watson <neil@watson-wilson.ca>
2015-07-29 07:18:04 -0400

    Separate std options to a module for later expansion

--- perl/nhw.pl
+++ perl/nhw.pl
@@ -16,11 +16,6 @@ my $VERSION = 1;
 #
 sub _get_cli_args {
 
-   # Set default CLI args here. Getopts will override.
-   my $cli_arg_ref = {
-      myarg => 'default', # Sample can be removed
-   };
-
    # Define ways to valid your arguments using anonymous subs or regexes.
    # These are samples that you can remove.
    my $valid_arg_ref = {
@@ -36,30 +31,24 @@ sub _get_cli_args {
       }
    };
 
+   # Get standard options, that all progs should have, from reusable module
+   my $options = Std::Opts->new();
+   my $std_cli_arg_ref = $options->get_standard_args;
+
+   # Set default CLI args here. Getopts will override.
+   my $cli_arg_ref = {
+      myarg => 'default', # Sample can be removed
+   };
+
    # Read, process, and validate cli args
    GetOptions(
       $cli_arg_ref,
+      %{ $std_cli_arg_ref },
 
       # These two samples can be removed
       'myarg=s',
       'arg2=i',
 
-      'version'  => sub { say $VERSION; exit                            },
-      'test'     => sub { _run_tests(); exit                            },
-      'man'      => sub { pod2usage( -verbose => 2, -exitval => 0 )     },
-
-      'dumpargs' => sub {
-         say '$cli_arg_ref = '. Dumper( $cli_arg_ref ); exit
-      },
-      'help|?'   => sub {
-         pod2usage( -sections => ['OPTIONS'],  -exitval => 0, -verbose => 99)
-      },
-      'usage'    => sub {
-         pod2usage( -sections => ['SYNOPSIS'], -exitval => 0, -verbose => 99)
-      },
-      'examples' => sub {
-         pod2usage( -sections => 'EXAMPLES',   -exitval => 0, -verbose => 99)
-      },
    );
 
    # Futher, more complex cli arg validation
@@ -215,8 +204,6 @@ sub _test_cli_arg2_failure{
 #
 my $cli_arg_ref = _get_cli_args();
 
-__END__
-
 #
 # POD
 #
@@ -273,3 +260,106 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 =cut
+
+#
+# Modules
+#
+package Std::Opts;
+
+use strict;
+use warnings;
+
+sub new {
+   my ( $class ) = @_;
+   return bless {}, $class;
+}
+
+sub get_standard_args {
+   my $self = shift;
+
+   my $std_cli_arg_ref = {
+      'version'  => sub { say $VERSION; exit                              },
+      'test'     => sub { main::_run_tests(); exit                        },
+      'man'      => sub { main::pod2usage( -verbose => 2, -exitval => 0 ) },
+
+      'dumpargs' => sub {
+         say '$cli_arg_ref = '. main::Dumper( $cli_arg_ref ); exit
+      },
+      'help|?'   => sub {
+         main::pod2usage( -sections => ['OPTIONS'],  -exitval => 0,
+            -verbose => 99)
+      },
+      'usage'    => sub {
+         main::pod2usage( -sections => ['SYNOPSIS'], -exitval => 0,
+            -verbose => 99)
+      },
+      'examples' => sub {
+         main::pod2usage( -sections => 'EXAMPLES',   -exitval => 0,
+            -verbose => 99)
+      },
+   };
+
+   return $std_cli_arg_ref;
+}
+
+=pod
+
+=head1 NAME
+
+Std::Opts - A module to hand standard cli options like man, help, usage, 
+version, and examples.
+
+=head1 SYNOPSIS
+
+Std::Opts contains standard CLI options that all your programs and scripts
+should contain. In a corporate environment you might separate this module
+and install it properly. You could also add to is or extend it with sub 
+modules.
+
+Your program will require Getop::long, Data::Dumper, and Pod::Usage. 
+
+   my $options = Std::Opts->new();
+   my $std_cli_arg_ref = $options->get_standard_args;
+
+   # Read, process, and validate cli args
+   GetOptions(
+      $cli_arg_ref,
+      %{ $std_cli_arg_ref },
+
+      # Custom args for your program here
+      'myarg=s',
+      'arg2=i',
+
+   );
+
+=head1 AUTHOR
+
+Neil H. Watson, http://watson-wilson.ca, C<< <neil@watson-wilson.ca> >>
+
+=head1 LICENSE and COPYRIGHT
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Neil H Watson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+=cut
+1;
