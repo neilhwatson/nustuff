2d7c7d42661f2da2cbc2d96d432b786180530bae
Neil H watson <neil@watson-wilson.ca>
2017-01-31 16:20:49 -0500

    Style fix

--- python/class.py
+++ python/class.py
@@ -12,29 +12,29 @@ class MyPerson(object):
     """Make empoyee person class with names, and title required."""
 
     def __init__(self, given_name, surname, title, salary=0):
-        self.given_name =given_name
-        self.surname    =surname
-        self.title      =title
-        self.salary     =salary
+        self.given_name = given_name
+        self.surname    = surname
+        self.title      = title
+        self.salary     = salary
 
-        if not isinstance( salary, ( int, long ) ):
-            raise TypeError( "Error salary must be an integer" )
-
+        if not isinstance(salary, (int, long)):
+            raise TypeError("Error salary must be an integer")
 
     def salary_pretty(self):
         """Return salary in pretty $000.00 format"""
-        return '$'+ str(self.salary) +'.00'
+        return '$' + str(self.salary) + '.00'
 
-person=MyPerson( given_name='neil', surname='watson', title='sysadmin')
 
-print 'Name is '+ person.given_name +' '+ person.surname
+person = MyPerson(given_name='neil', surname='watson', title='sysadmin')
+
+print 'Name is ' + person.given_name + ' ' + person.surname
 
 # Python has no simple methods for making attributes read-only :(
 
-print 'Title is '+ person.title
-person.title='Manager'
-print 'Title is now '+ person.title
+print 'Title is ' + person.title
+person.title = 'Manager'
+print 'Title is now ' + person.title
 
-person.salary=10000
+person.salary = 10000
 print 'salary is', person.salary
-print'Formatted salary is '+ person.salary_pretty()
+print'Formatted salary is ' + person.salary_pretty()
