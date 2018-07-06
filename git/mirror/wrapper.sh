40aad7908988651bfdae3e4c5b9449971a6044a2
Neil H watson <neil@watson-wilson.ca>
2018-07-05 20:37:11 -0400

    Add new repos and enhance allowed repo name syntax

--- git/mirror/wrapper.sh
+++ git/mirror/wrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-repos="vim_cf3 cfbot nustuff delta_reporting evolve_cfengine_freelib terraform-modules"
+repos="vim_cf3 cfbot nustuff delta_reporting evolve_cfengine_freelib terraform-modules thewaves www.watson-wilson.ca"
 
 for next_repo in $repos
 do
-   echo ./gitmirror.py -g2b neilhwatson/$next_repo neilhwatson/$next_repo
-   ./gitmirror.py -g2b neilhwatson/$next_repo neilhwatson/$next_repo
+   echo ./gitmirror.py -g2b "neilhwatson/$next_repo" "neilhwatson/$next_repo"
+   ./gitmirror.py -g2b "neilhwatson/$next_repo" "neilhwatson/$next_repo"
 done
