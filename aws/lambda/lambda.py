de349cc56ec158ba181d12b288817e0d0df87e13
Neil H watson <neil@watson-wilson.ca>
2018-07-21 08:15:36 -0400

    Add some comments to the lambda example

--- aws/lambda/lambda.py
+++ aws/lambda/lambda.py
@@ -1,6 +1,8 @@
 def my_lambda(event, context):
+    '''The name of this sub must match the lambda handler, and the two 
+    parameters are required.'''
     
-
+    # Return whatever you like.
     return [ 
              { "test": "one", "result": "pass" },
              { "test": "two", "result": "pass" }
