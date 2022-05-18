fe293a9f2788c985fac9bd30a1505357126d19b0
Neil H watson <neil@watson-wilson.ca>
2022-05-18 15:09:07 -0400

    export iam policies

---
+++ terraform/get-iam-policy/main.tf
@@ -0,0 +1,23 @@
+variable policy_arns { type = list }
+
+data aws_iam_policy p01 {
+   count = length(var.policy_arns)
+   arn   = var.policy_arns[count.index]
+}
+
+locals {
+
+   policies = [
+      for p in data.aws_iam_policy.p01 : {
+         name = p.name
+         path = p.path
+         description = p.description
+         policy = jsondecode(p.policy)
+      }
+   ]
+}
+
+output policies {
+   value = local.policies
+}
+
