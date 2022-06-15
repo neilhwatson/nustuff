1bf2be37b87843740761e62d0094611373f35ce7
Neil H watson <neil@watson-wilson.ca>
2022-06-15 09:34:21 -0400

    fix names

--- inspec/vpc/controls/vpc.rb
+++ inspec/vpc/controls/vpc.rb
@@ -1,18 +1,18 @@
 # This finds the default VPC
 # See https://docs.chef.io/inspec/resources/aws_vpc/
 
-describe aws_vpc('vpc-068a5ae7b3e423c15') do
+describe aws_vpc('vpc-3497eafeoin3048') do
   it { should exist }
   it { should be_available }
   its('tags') { should include(
-    'Name'    => 'clutch-main',
+    'Name'    => 'main',
     'owner'   => 'devops',
     'purpose' => 'general use'
   ) }
   its('cidr_block') { should cmp '10.88.0.0/16' }
 end
 
-describe aws_subnets.where(vpc_id: 'vpc-068a5ae7b3e423c15') do
+describe aws_subnets.where(vpc_id: 'vpc-3497eafeoin3048') do
   it { should exist }
   its('availability_zone') { should include('ca-central-1a') }
   its('availability_zone') { should include('ca-central-1b') }
