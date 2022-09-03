6315abc45ca12bd23addae6195bb96abc52461e6
Neil H watson <neil@watson-wilson.ca>
2022-09-03 19:13:40 -0400

    add requests example

--- kafka/c.py
+++ kafka/c.py
@@ -5,8 +5,8 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
 consumer.subscribe(['nwatson'])
-pprint(vars(consumer))
 
 for next_msg in consumer:
     print(next_msg)
+    print(str(next_msg.value))
 
--- kafka/p.py
+++ kafka/p.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 
 from kafka import KafkaProducer
+import re
+import requests
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-future = producer.send('nwatson', b'hello world')
-result = future.get(timeout=4)
+config = {
+    'github': {
+        'url': 'https://www.githubstatus.com/',
+        'body': 'All Systems Operational',
+        'timeout': 5 # TODO make a default timeout
+    }
+}
+
+def test(url
+for next_topic in config.keys():
+    # Note that measuring total response time we may be measruing DNS too
+    r = requests.get(config[next_topic]['url'], timeout=config[next_topic]['timeout'])
+
+    # MVP+1 no blocking checks
+    print(r.status_code)
+    print(r.elapsed)
+    if re.search(config[next_topic]['body'], r.text):
+        print('body OK')
+
+# future = producer.send('nwatson', b'hello world')
+# result = future.get(timeout=4)
