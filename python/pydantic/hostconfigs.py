fc73358b1f62e124a7fcae7604b27bddd6c8ca46
Neil H watson <neil@watson-wilson.ca>
2023-03-20 21:39:58 -0400

    pydantic exmample

---
+++ python/pydantic/hostconfigs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+from pydantic import BaseModel
+from typing import List, Dict
+
+configs = [
+    {
+        "host": "mars",
+        "packages": ["vim", "git", "tmux"],
+        "users": [
+            {
+                "name": "neil",
+                "groups": ["wheel", "docker"],
+            }
+        ]
+    },
+    {
+        "host": "venus",
+        "packages": ["apache", "php"],
+        "users": [
+            {
+                "name": "httpd",
+                "groups": ["httpd"],
+            }
+        ]
+    }
+]
+
+class UserModel(BaseModel):
+    name: str
+    groups: List[str]
+
+
+class HostModel(BaseModel):
+    host: str
+    users: list[UserModel]
+    packages: list[str]
+
+class ConfigModel(BaseModel):
+    hosts: list[HostModel]
+
+c = ConfigModel(hosts=configs)
+
+for host in c.hosts:
+    print(host.host)
+    for user in host.users:
+        print(user.name)
+        print(user.groups)
