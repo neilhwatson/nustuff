943a4c5d16cd4b33ff757a2b42129f3e572d9a08
Neil H watson <neil@watson-wilson.ca>
2023-03-23 18:52:11 -0400

    add constructor

--- python/pydantic/hostconfigs.py
+++ python/pydantic/hostconfigs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import Any, List, Dict
 
 configs = [
     {
@@ -35,16 +35,20 @@ class HostModel(BaseModel):
     host: str
     users: list[UserModel]
     packages: list[str]
+    print("class")
 
-class ConfigModel(BaseModel):
-    hosts: list[HostModel]
+    @staticmethod
+    def get_hosts(hosts: Any) -> list["HostModel"]:
+        print("get_hosts")
+        class ConfigModel(BaseModel):
+            __root__: list[HostModel]
+        return ConfigModel.parse_obj(hosts).__root__
 
-c = ConfigModel(hosts=configs)
 
+c = HostModel.get_hosts(hosts=configs)
 print(c)
-print("first username in first host ", c.hosts[0].users[0].name)
-print("hosts ", c.hosts)
-for host in c.hosts:
+print("first username in first host ", c[0].users[0].name)
+for host in c:
     print(host.host)
     for user in host.users:
         print(user.name)
