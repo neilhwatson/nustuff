41d719fca7f5fba1765780171eb7da0bef98d1bb
Neil H Watson <neil@watson-wilson.ca>
2025-11-28 20:14:07 -0500

    remove widget

--- X/awesome/rc.lua
+++ X/awesome/rc.lua
@@ -278,7 +278,7 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             spacing = 10,
             wibox.widget.systray(),
-            weather,
+            -- weather,
             weather2,
             batwidget,
             mytextclock,
